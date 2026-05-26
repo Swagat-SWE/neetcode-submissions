b5470666377ad21cbf32c926ead26176b541755a
Swagat Karki <karkiswagat10@gmail.com>
2026-05-25 20:19:01 -0500

    Add: python-read-input-practice - submission-1

---
+++ Python For Beginners/python-read-input-practice/submission-1.py
@@ -0,0 +1,13 @@
+def add_two_numbers() -> int:
+    val = input()
+    slt = val.split(",")
+
+    num1= int(slt[0])
+    num2= int(slt[1])
+    return num1 + num2
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
